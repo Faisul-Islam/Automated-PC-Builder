50db2f756e6766648772bd7163e96121b1f175ff
Faisul-Islam <suhid15@gmail.com>
2022-04-12 01:28:49 +0600

    fixed iGPU detection for amd CPUS

--- src/main/java/com/apcb/demo/controller/APCBController.java
+++ src/main/java/com/apcb/demo/controller/APCBController.java
@@ -1,16 +1,10 @@
 package com.apcb.demo.controller;
 
-import com.apcb.demo.dto.response.CPUInitialResponse;
 import com.apcb.demo.dto.response.PCResponse;
 import com.apcb.demo.services.APCBService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/apcb/")
@@ -22,6 +16,7 @@ public class APCBController {
         this.apcbService = apcbService;
     }
 
+    @CrossOrigin("https://auto-pc-builder.netlify.app")
     @GetMapping(path ="/get/b={brand}&p={price}")
     public ResponseEntity<PCResponse> getCPUInfo(@PathVariable String brand, @PathVariable int price){
         return new ResponseEntity(apcbService.getPCInfo(brand,price), HttpStatus.OK);
